fec10e76851b098950e64998faa6460e0f374434
Victor Nguyen <victorpublic0000@gmail.com>
2024-02-14 20:44:46 +0700

    feat: finish home screen design

---
+++ apps/driving_license/lib/features/home/presentation/chapter_card.dart
@@ -0,0 +1,68 @@
+import 'package:driving_license/common_widgets/button_card.dart';
+import 'package:driving_license/constants/app_sizes.dart';
+import 'package:driving_license/utils/context_ext.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:gap/gap.dart';
+import 'package:material_symbols_icons/symbols.dart';
+import 'package:vector_graphics/vector_graphics.dart';
+
+class ChapterCard extends StatelessWidget {
+  final String iconAssetPath;
+  final String title;
+  final String subhead;
+
+  const ChapterCard({
+    super.key,
+    required this.iconAssetPath,
+    required this.title,
+    required this.subhead,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ButtonCard(
+      surfaceColor: context.materialScheme.surfaceContainerHigh,
+      onSurfaceColor: context.materialScheme.onSurface,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: SizeConstant.p16,
+          vertical: SizeConstant.p12,
+        ),
+        child: Row(
+          children: [
+            SvgPicture(AssetBytesLoader(iconAssetPath)),
+            const Gap(SizeConstant.p16),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: context.textTheme.titleMedium,
+                  ),
+                  const Gap(SizeConstant.p2),
+                  Text(
+                    subhead,
+                    style: context.textTheme.bodyMedium!.copyWith(
+                      color: context.colorScheme.onSurfaceVariant,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Gap(SizeConstant.p16),
+            Icon(
+              Symbols.arrow_forward_ios,
+              size: SizeConstant.p20,
+              color: context.colorScheme.secondary,
+            ),
+          ],
+        ),
+      ),
+      onTap: () {
+        // debugPrint(IconTheme.of(context).opticalSize.toString());
+      },
+    );
+  }
+}
--- apps/driving_license/lib/features/home/presentation/home_screen.dart
+++ apps/driving_license/lib/features/home/presentation/home_screen.dart
@@ -1,7 +1,9 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:driving_license/constants/app_sizes.dart';
+import 'package:driving_license/features/home/presentation/chapter_card.dart';
 import 'package:driving_license/features/home/presentation/donate_card.dart';
 import 'package:driving_license/features/home/presentation/feature_card.dart';
+import 'package:driving_license/utils/context_ext.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_layout_grid/flutter_layout_grid.dart';
 import 'package:gap/gap.dart';
@@ -17,66 +19,154 @@ class HomeScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        toolbarHeight: 64,
-        titleSpacing: 0,
+        backgroundColor: AppBarBackgroundColor(context),
         leading: IconButton(
-          icon: const Icon(Symbols.menu, size: PaddingSize.p24),
+          icon: const Icon(
+            Symbols.menu,
+            size: SizeConstant.p20,
+          ),
           onPressed: () {},
         ),
         title: const Text(licenseName),
         actions: [
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: PaddingSize.p12),
+            padding: const EdgeInsets.symmetric(horizontal: SizeConstant.p12),
             child: TextButton(onPressed: () {}, child: const Text('Báo lỗi')),
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(
-          left: PaddingSize.p16,
-          right: PaddingSize.p16,
-          top: PaddingSize.p4,
-          bottom: PaddingSize.p48,
+      body: const SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.only(
+            left: SizeConstant.p16,
+            right: SizeConstant.p16,
+            top: SizeConstant.p4,
+            bottom: SizeConstant.p48,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              DonateCard(),
+              Gap(SizeConstant.p20),
+              FeatureSelection(),
+              Gap(SizeConstant.p32),
+              ChapterSelection(),
+            ],
+          ),
         ),
-        child: Column(
-          children: [
-            const DonateCard(),
-            const Gap(PaddingSize.p20),
-            LayoutGrid(
-              columnSizes: [1.fr, 1.fr],
-              rowSizes: const [auto, auto],
-              rowGap: PaddingSize.p12,
-              columnGap: PaddingSize.p12,
-              children: const [
-                SizedBox.expand(
-                  child: FeatureCard(
-                    title: 'Thi thử',
-                    subhead: 'Bộ đề được tạo ra ngẫu nhiên',
-                  ),
-                ),
-                SizedBox.expand(
-                  child: FeatureCard(
-                    title: 'Các câu khó',
-                    subhead: '50 câu hỏi dễ bị nhầm lẫn',
-                  ),
-                ),
-                SizedBox.expand(
-                  child: FeatureCard(
-                    title: 'Đã lưu',
-                    subhead: 'Những câu hỏi được đánh dấu lưu',
-                  ),
-                ),
-                SizedBox.expand(
-                  child: FeatureCard(
-                    title: 'Đã làm sai',
-                    subhead: 'Những câu hỏi bạn đã làm sai',
-                  ),
-                ),
-              ],
+      ),
+    );
+  }
+}
+
+class AppBarBackgroundColor extends MaterialStateColor {
+  final BuildContext context;
+  AppBarBackgroundColor(this.context)
+      : super(context.materialScheme.surface.value);
+
+  @override
+  Color resolve(Set<MaterialState> states) {
+    if (states.contains(MaterialState.scrolledUnder)) {
+      return context.materialScheme.surfaceContainerLow;
+    }
+    return context.materialScheme.surface;
+  }
+}
+
+class FeatureSelection extends StatelessWidget {
+  const FeatureSelection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutGrid(
+      columnSizes: [1.fr, 1.fr],
+      rowSizes: const [auto, auto],
+      rowGap: SizeConstant.p12,
+      columnGap: SizeConstant.p12,
+      children: const [
+        SizedBox.expand(
+          child: FeatureCard(
+            title: 'Thi thử',
+            subhead: 'Bộ đề được tạo ra ngẫu nhiên',
+          ),
+        ),
+        SizedBox.expand(
+          child: FeatureCard(
+            title: 'Các câu khó',
+            subhead: '50 câu hỏi dễ bị nhầm lẫn',
+          ),
+        ),
+        SizedBox.expand(
+          child: FeatureCard(
+            title: 'Đã lưu',
+            subhead: 'Những câu hỏi được đánh dấu lưu',
+          ),
+        ),
+        SizedBox.expand(
+          child: FeatureCard(
+            title: 'Đã làm sai',
+            subhead: 'Những câu hỏi bạn đã làm sai',
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ChapterSelection extends StatelessWidget {
+  const ChapterSelection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('Ôn tập câu hỏi', style: context.textTheme.titleLarge),
+        const Gap(SizeConstant.p16),
+        LayoutGrid(
+          // 1 column
+          columnSizes: [1.fr],
+          // 6 rows
+          rowSizes: List.generate(6, (_) => auto),
+          rowGap: SizeConstant.p12,
+          children: const [
+            ChapterCard(
+              iconAssetPath:
+                  'assets/icons/home_screen/complied/danger_fire.svg.vec',
+              title: 'Câu hỏi điểm liệt',
+              subhead: 'Đã hoàn thành 0 / 20',
+            ),
+            ChapterCard(
+              iconAssetPath: 'assets/icons/home_screen/complied/books.svg.vec',
+              title: 'Khái niệm và quy tắc',
+              subhead: 'Đã hoàn thành 0 / 83 - Sai 5 câu',
+            ),
+            ChapterCard(
+              iconAssetPath: 'assets/icons/home_screen/complied/person.svg.vec',
+              title: 'Văn hoá và đạo đức',
+              subhead: 'Đã hoàn thành 0 / 5',
+            ),
+            ChapterCard(
+              iconAssetPath:
+                  'assets/icons/home_screen/complied/steering_wheel.svg.vec',
+              title: 'Kỹ thuật lái xe',
+              subhead: 'Đã hoàn thành 0 / 12',
+            ),
+            ChapterCard(
+              iconAssetPath:
+                  'assets/icons/home_screen/complied/turn_right_sign.svg.vec',
+              title: 'Biển báo đường bộ',
+              subhead: 'Đã hoàn thành 0 / 65',
+            ),
+            ChapterCard(
+              iconAssetPath:
+                  'assets/icons/home_screen/complied/traffic_light.svg.vec',
+              title: 'Sa hình',
+              subhead: 'Đã hoàn thành 0 / 35',
             ),
           ],
         ),
-      ),
+      ],
     );
   }
 }
