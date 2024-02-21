ef8fd0bddb973190b00af0eb843bf1c0dc612df3
Victor Nguyen <victorpublic0000@gmail.com>
2024-02-21 15:58:04 +0700

    feat: introduce explanation and remember tip cards to QuestionPage

---
+++ apps/driving_license/lib/features/question/presentation/answer/remember_tip_card.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RememberTipCard extends StatelessWidget {
+  final String content;
+
+  const RememberTipCard({super.key, required this.content});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('hello');
+  }
+}
--- apps/driving_license/lib/features/question/presentation/question_page.dart
+++ apps/driving_license/lib/features/question/presentation/question_page.dart
@@ -2,6 +2,8 @@ import 'package:driving_license/constants/app_sizes.dart';
 import 'package:driving_license/constants/gap_sizes.dart';
 import 'package:driving_license/features/question/data/question_repository.dart';
 import 'package:driving_license/features/question/presentation/answer/answer_card_list.dart';
+import 'package:driving_license/features/question/presentation/answer/explanation_card.dart';
+import 'package:driving_license/features/question/presentation/answer/remember_tip_card.dart';
 import 'package:driving_license/utils/context_ext.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -40,9 +42,9 @@ class QuestionPage extends HookConsumerWidget {
               style: context.textTheme.titleMedium,
             ),
             kGap_16,
-            AnswerCardList(
-              questionIndex: questionIndex,
-            ),
+            AnswerCardList(questionIndex: questionIndex),
+            QuestionNotes(questionIndex: questionIndex),
+            kGap_48,
           ],
         ),
       ),
@@ -61,6 +63,37 @@ extension QuestionPageX on QuestionPage {
           .read(questionPageScrollControllerProvider(questionIndex).notifier)
           .value = scrollController;
     });
+  }
+}
+
+class QuestionNotes extends HookConsumerWidget {
+  final int questionIndex;
+  const QuestionNotes({super.key, required this.questionIndex});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final question =
+        ref.watch(questionRepositoryProvider).getQuestion(questionIndex);
+
+    final content = [
+      if (question.explanation != null)
+        ExplanationCard(content: question.explanation!),
+      if (question.rememberTip != null)
+        RememberTipCard(content: question.rememberTip!),
+    ];
+
+    return Column(
+      children: [
+        if (content.isNotEmpty) kGap_24,
+        ListView.separated(
+          physics: const NeverScrollableScrollPhysics(),
+          shrinkWrap: true,
+          separatorBuilder: (BuildContext context, int index) => kGap_16,
+          itemCount: content.length,
+          itemBuilder: (BuildContext _, int index) => content[index],
+        ),
+      ],
+    );
   }
 }
 
