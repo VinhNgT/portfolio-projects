5486333ff2e45c72bf0ee5925687f1541371af82
Victor Nguyen <victorpublic0000@gmail.com>
2024-02-25 23:21:57 +0700

    style: add golden ratios

--- apps/driving_license/lib/constants/opacity.dart
+++ apps/driving_license/lib/constants/opacity.dart
@@ -1,6 +1,10 @@
-/// Opacity values used in the app
+/// Constants for opacity values used in the UI
 library;
 
+import 'package:driving_license/constants/golden_ratios.dart';
+
 const kOpacityFull = 1.0;
-const kOpacityDisabled = 0.38;
+const kOpacityHalf = kGoldenRatioDivide_1;
+const kOpacityDisabled = kGoldenRatioDivide_2;
+const kOpacityQuarter = kGoldenRatioDivide_3;
 const kOpacityZero = 0.0;
