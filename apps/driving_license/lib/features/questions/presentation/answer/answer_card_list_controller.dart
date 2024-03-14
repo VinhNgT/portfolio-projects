990275443f1087d3f526ec7894c5256c9a579539
Victor Nguyen <victorpublic0000@gmail.com>
2024-03-14 09:08:22 +0700

    chore: remove SelectedAnswerIndexProvider

--- apps/driving_license/lib/features/questions/presentation/answer/answer_card_list_controller.dart
+++ apps/driving_license/lib/features/questions/presentation/answer/answer_card_list_controller.dart
@@ -4,18 +4,6 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'answer_card_list_controller.g.dart';
 
 @riverpod
-class SelectedAnswerIndex extends _$SelectedAnswerIndex {
-  @override
-  int? build(int questionPageIndex) {
-    return null;
-  }
-
-  set value(int? newValue) {
-    state = newValue;
-  }
-}
-
-@riverpod
 class AnswerCardListController extends _$AnswerCardListController {
   @override
   FutureOr<void> build() {}
