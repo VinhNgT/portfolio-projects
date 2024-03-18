fb4c7d2d47b24860ed0a4863ca1b01766fb37367
Victor Nguyen <victorpublic0000@gmail.com>
2024-03-18 09:59:53 +0700

    feat: add 'loadingWidget' parameter to AsyncValueWidget

--- apps/driving_license/lib/common_widgets/async_value/async_value_widget.dart
+++ apps/driving_license/lib/common_widgets/async_value/async_value_widget.dart
@@ -2,15 +2,30 @@ import 'package:driving_license/common_widgets/error_message_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+/// A widget that handles different states of an asynchronous value and builds
+/// the UI accordingly.
 class AsyncValueWidget<T> extends StatelessWidget {
-  final bool showLoadingIndicator;
   final AsyncValue<T> value;
+  final bool showLoadingIndicator;
+  final Widget? loadingWidget;
   final Widget Function(T) builder;
 
+  /// Creates an [AsyncValueWidget] with the given parameters.
+  ///
+  /// The [value] represents the asynchronous value to handle.
+  ///
+  /// The [showLoadingIndicator] determines whether to show the loading
+  /// indicator.
+  ///
+  /// The [loadingWidget] is the widget to display when the value is loading.
+  /// If this is provided, the [showLoadingIndicator] will be ignored.
+  ///
+  /// The [builder] is a function that builds the UI based on the data value.
   const AsyncValueWidget({
     super.key,
-    this.showLoadingIndicator = false,
     required this.value,
+    this.showLoadingIndicator = false,
+    this.loadingWidget,
     required this.builder,
   });
 
@@ -19,9 +34,20 @@ class AsyncValueWidget<T> extends StatelessWidget {
     return value.when(
       data: builder,
       error: (e, st) => Center(child: ErrorMessageWidget(e.toString())),
-      loading: () => showLoadingIndicator
-          ? const Center(child: CircularProgressIndicator())
-          : const SizedBox.shrink(),
+      loading: () =>
+          loadingWidget ?? _LoadingIndicator(hidden: !showLoadingIndicator),
     );
+  }
+}
+
+class _LoadingIndicator extends StatelessWidget {
+  final bool hidden;
+  const _LoadingIndicator({this.hidden = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return hidden
+        ? const SizedBox.shrink()
+        : const Center(child: CircularProgressIndicator());
   }
 }
