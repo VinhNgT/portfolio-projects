2862f095cf4fee7608095f249f7d5309490208a5
Victor Nguyen <victorpublic0000@gmail.com>
2024-03-22 10:21:51 +0700

    refactor: make Question class abstract

--- apps/driving_license/lib/features/questions/domain/question.dart
+++ apps/driving_license/lib/features/questions/domain/question.dart
@@ -4,7 +4,7 @@ part 'question.freezed.dart';
 part 'question.g.dart';
 
 @freezed
-class Question with _$Question {
+abstract class Question with _$Question {
   const factory Question({
     required int questionDbIndex,
     required int chapterDbIndex,
