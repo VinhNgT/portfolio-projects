715535f3659a93c8f82e73bfa5621c40ac66b1a3
Victor Nguyen <victorpublic0000@gmail.com>
2024-03-23 13:40:29 +0700

    feat: create ChapterProgressService

---
+++ apps/driving_license/lib/features/chapters/application/chapter_progress_service.dart
@@ -0,0 +1,95 @@
+import 'package:driving_license/features/chapters/domain/chapter.dart';
+import 'package:driving_license/features/questions/data/question/questions_repository.dart';
+import 'package:driving_license/features/questions/data/user_answer/user_answers_repository.dart';
+import 'package:driving_license/features/result/domain/test_result.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+part 'chapter_progress_service.g.dart';
+
+class ChapterProgressService {
+  final Chapter chapter;
+  final UserAnswersRepository userAnswersRepository;
+  final QuestionsRepository questionsRepository;
+  final SharedPreferences prefs;
+
+  ChapterProgressService({
+    required this.chapter,
+    required this.userAnswersRepository,
+    required this.questionsRepository,
+    required this.prefs,
+  });
+
+  Stream<TestResult> watchChapterCompletionStatus() async* {
+    final chapterQuestionsCount =
+        await questionsRepository.getQuestionCountByChapter(chapter);
+    final userAnswersCountStream =
+        userAnswersRepository.watchChapterAnswersCount(chapter);
+    final wrongUserAnswersCountStream =
+        userAnswersRepository.watchChapterWrongAnswersCount(chapter);
+
+    yield* Rx.combineLatest2(
+      userAnswersCountStream,
+      wrongUserAnswersCountStream,
+      (int userAnswersCount, int wrongUserAnswersCount) {
+        return TestResult(
+          totalQuestions: chapterQuestionsCount,
+          answeredQuestions: userAnswersCount,
+          correctAnswers: userAnswersCount - wrongUserAnswersCount,
+          wrongAnswers: wrongUserAnswersCount,
+        );
+      },
+    );
+  }
+
+  Future<void> saveLastQuestionPageIndexVisited(int questionPageIndex) async {
+    await prefs.setInt(
+      'chapter_last_question_${chapter.name}',
+      questionPageIndex,
+    );
+  }
+
+  Future<int> getLastQuestionPageIndexVisited() async {
+    return prefs.getInt('chapter_last_question_${chapter.name}') ?? 0;
+  }
+}
+
+@riverpod
+FutureOr<ChapterProgressService> chapterProgressService(
+  ChapterProgressServiceRef ref,
+  Chapter chapter,
+) async {
+  final userAnswersRepository = ref.watch(userAnswersRepositoryProvider);
+  final questionsRepository = ref.watch(questionsRepositoryProvider);
+  final prefs = await SharedPreferences.getInstance();
+
+  return ChapterProgressService(
+    chapter: chapter,
+    userAnswersRepository: userAnswersRepository,
+    questionsRepository: questionsRepository,
+    prefs: prefs,
+  );
+}
+
+@riverpod
+Stream<TestResult> chapterCompletionStatus(
+  ChapterCompletionStatusRef ref,
+  Chapter chapter,
+) async* {
+  final chapterProgressService =
+      await ref.watch(chapterProgressServiceProvider(chapter).future);
+
+  yield* chapterProgressService.watchChapterCompletionStatus();
+}
+
+@riverpod
+FutureOr<int> lastQuestionPageIndexVisited(
+  LastQuestionPageIndexVisitedRef ref,
+  Chapter chapter,
+) async {
+  final chapterProgressService =
+      await ref.watch(chapterProgressServiceProvider(chapter).future);
+
+  return chapterProgressService.getLastQuestionPageIndexVisited();
+}
--- apps/driving_license/lib/features/home/presentation/chapter_card.dart
+++ apps/driving_license/lib/features/home/presentation/chapter_card.dart
@@ -2,7 +2,7 @@ import 'package:driving_license/common_widgets/async_value/async_value_widget.da
 import 'package:driving_license/common_widgets/button_card.dart';
 import 'package:driving_license/constants/app_sizes.dart';
 import 'package:driving_license/constants/gap_sizes.dart';
-import 'package:driving_license/features/chapters/application/chapter_completion_status_provider.dart';
+import 'package:driving_license/features/chapters/application/chapter_progress_service.dart';
 import 'package:driving_license/features/chapters/domain/chapter.dart';
 import 'package:driving_license/features/result/domain/test_result.dart';
 import 'package:driving_license/utils/context_ext.dart';
