61d7542bb5cbd76aac46174a964954e92005544c
Victor Nguyen <victorpublic0000@gmail.com>
2024-03-27 11:48:30 +0700

    add UserSelectedLicense provider

---
+++ apps/driving_license/lib/features/licenses/data/providers/user_selected_license_provider.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_selected_license_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$userSelectedLicenseHash() =>
+    r'e55ddb9f4bcc4c405a2b25a1f144667b66f1f531';
+
+/// See also [UserSelectedLicense].
+@ProviderFor(UserSelectedLicense)
+final userSelectedLicenseProvider =
+    AutoDisposeAsyncNotifierProvider<UserSelectedLicense, License>.internal(
+  UserSelectedLicense.new,
+  name: r'userSelectedLicenseProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$userSelectedLicenseHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$UserSelectedLicense = AutoDisposeAsyncNotifier<License>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
