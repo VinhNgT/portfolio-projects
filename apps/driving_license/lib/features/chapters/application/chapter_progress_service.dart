b728dff276d1c901ff9e086c500ccd0201b2ca04
Victor Nguyen <victorpublic0000@gmail.com>
2024-03-28 13:34:09 +0700

    chore: remove SharedPreferences from ChapterProgressService

--- apps/driving_license/lib/features/chapters/application/chapter_progress_service.dart
+++ apps/driving_license/lib/features/chapters/application/chapter_progress_service.dart
@@ -6,7 +6,6 @@ import 'package:driving_license/features/questions/data/user_answer/user_answers
 import 'package:driving_license/features/result/domain/test_result.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:rxdart/rxdart.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 part 'chapter_progress_service.g.dart';
 
@@ -15,14 +14,12 @@ class ChapterProgressService {
   final Chapter chapter;
   final UserAnswersRepository userAnswersRepository;
   final QuestionsRepository questionsRepository;
-  final SharedPreferences prefs;
 
   ChapterProgressService({
     required this.license,
     required this.chapter,
     required this.userAnswersRepository,
     required this.questionsRepository,
-    required this.prefs,
   });
 
   Stream<TestResult> watchChapterCompletionStatus() async* {
@@ -57,13 +54,11 @@ FutureOr<ChapterProgressService> chapterProgressService(
       await ref.watch(userSelectedLicenseProvider.future);
   final userAnswersRepository = ref.watch(userAnswersRepositoryProvider);
   final questionsRepository = ref.watch(questionsRepositoryProvider);
-  final prefs = await SharedPreferences.getInstance();
 
   return ChapterProgressService(
     license: userSelectedLicense,
     chapter: chapter,
     userAnswersRepository: userAnswersRepository,
     questionsRepository: questionsRepository,
-    prefs: prefs,
   );
 }
