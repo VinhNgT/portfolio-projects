ff2ffbcbe6d5c521e290ba59b6ea6eb4faf0e93b
Victor Nguyen <victorpublic0000@gmail.com>
2024-03-29 22:34:39 +0700

    feat: add License iconAssetPath

--- apps/driving_license/lib/features/licenses/domain/license.dart
+++ apps/driving_license/lib/features/licenses/domain/license.dart
@@ -1,9 +1,12 @@
 enum License {
-  all,
-  a1,
-  a2,
-  a3,
-  a4,
-  b1,
-  b2,
+  all('assets/icons/home_screen/compiled/danger_fire.svg.vec'),
+  a1('assets/icons/vehicles/compiled/motorbike-1.svg.vec'),
+  a2('assets/icons/vehicles/compiled/motorbike-2.svg.vec'),
+  a3('assets/icons/vehicles/compiled/motorbike-scooter.svg.vec'),
+  a4('assets/icons/vehicles/compiled/loader-truck.svg.vec'),
+  b1('assets/icons/vehicles/compiled/car.svg.vec'),
+  b2('assets/icons/vehicles/compiled/taxi.svg.vec');
+
+  const License(this.iconAssetPath);
+  final String iconAssetPath;
 }
