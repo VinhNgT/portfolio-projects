b206fabdf05b2e96c188df0248375f0766343e8b
Victor Nguyen <victorpublic0000@gmail.com>
2024-03-30 13:34:17 +0700

    feat: use FadeThroughTransition when navigating with LicenseSelectionRoute

--- apps/driving_license/lib/routing/app_router.dart
+++ apps/driving_license/lib/routing/app_router.dart
@@ -2,6 +2,7 @@ import 'package:animations/animations.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:driving_license/features/home/presentation/home_screen.dart';
 import 'package:driving_license/routing/app_router.gr.dart';
+import 'package:driving_license/routing/previous_route_observer.dart';
 import 'package:flutter/material.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -14,27 +15,65 @@ class AppRouter extends $AppRouter {
 
   @override
   List<AutoRoute> get routes => [
-        AutoRoute(
+        CustomRoute(
           page: HomeRoute.page,
           guards: [HomeRouteGuard(ref)],
+          transitionsBuilder: _licenseSelectionFromToTransition,
           initial: true,
         ),
         AutoRoute(page: QuestionRoute.page),
-        AutoRoute(page: LicenseSelectionRoute.page),
+        CustomRoute(
+          page: LicenseSelectionRoute.page,
+          transitionsBuilder: _licenseSelectionFromToTransition,
+        ),
       ];
 
   @override
-  RouteType get defaultRouteType => RouteType.custom(
-        transitionsBuilder: (context, animation, secondaryAnimation, child) {
-          return SharedAxisTransition(
+  RouteType get defaultRouteType =>
+      RouteType.custom(transitionsBuilder: _defaultTransition);
+}
+
+extension _TransitionX on AppRouter {
+  RouteTransitionsBuilder get _defaultTransition => (
+        context,
+        animation,
+        secondaryAnimation,
+        child,
+      ) =>
+          SharedAxisTransition(
             fillColor: Theme.of(context).scaffoldBackgroundColor,
             animation: animation,
             secondaryAnimation: secondaryAnimation,
             transitionType: SharedAxisTransitionType.horizontal,
             child: child,
           );
-        },
-      );
+
+  RouteTransitionsBuilder get _licenseSelectionFromToTransition => (
+        context,
+        animation,
+        secondaryAnimation,
+        child,
+      ) {
+        final previousRouteName = ref.read(previousRouteObserverProvider).name;
+        final currentRouteName = context.topRoute.name;
+
+        if (previousRouteName == LicenseSelectionRoute.name ||
+            currentRouteName == LicenseSelectionRoute.name) {
+          return FadeThroughTransition(
+            fillColor: Theme.of(context).scaffoldBackgroundColor,
+            animation: animation,
+            secondaryAnimation: secondaryAnimation,
+            child: child,
+          );
+        }
+
+        return _defaultTransition(
+          context,
+          animation,
+          secondaryAnimation,
+          child,
+        );
+      };
 }
 
 @Riverpod(keepAlive: true)
