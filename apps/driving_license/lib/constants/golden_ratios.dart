092475f882a27451d268192f5816b8b224a86fb0
Victor Nguyen <victorpublic0000@gmail.com>
2024-04-04 17:13:49 +0700

    fix: comment in golden_ratios.dart

--- apps/driving_license/lib/constants/golden_ratios.dart
+++ apps/driving_license/lib/constants/golden_ratios.dart
@@ -6,24 +6,32 @@ library;
 
 const double _kGoldenRatio = 1.61803398875;
 
-// 1.61803398875
+/// 1.61803398875
 const double kGoldenRatioMultiply_1 = 1 * _kGoldenRatio;
-// 2.61803398875
+
+/// 2.61803398875
 const double kGoldenRatioMultiply_2 = kGoldenRatioMultiply_1 * _kGoldenRatio;
-// 4.2360679775
+
+/// 4.2360679775
 const double kGoldenRatioMultiply_3 = kGoldenRatioMultiply_2 * _kGoldenRatio;
-// 6.85410196625
+
+/// 6.85410196625
 const double kGoldenRatioMultiply_4 = kGoldenRatioMultiply_3 * _kGoldenRatio;
-// 11.09016994375
+
+/// 11.09016994375
 const double kGoldenRatioMultiply_5 = kGoldenRatioMultiply_4 * _kGoldenRatio;
 
-// 0.61803398875
+/// 0.61803398875
 const double kGoldenRatioDivide_1 = 1 / _kGoldenRatio;
-// 0.38196601125
+
+/// 0.38196601125
 const double kGoldenRatioDivide_2 = kGoldenRatioDivide_1 / _kGoldenRatio;
-// 0.2360679775
+
+/// 0.2360679775
 const double kGoldenRatioDivide_3 = kGoldenRatioDivide_2 / _kGoldenRatio;
-// 0.14589803375
+
+/// 0.14589803375
 const double kGoldenRatioDivide_4 = kGoldenRatioDivide_3 / _kGoldenRatio;
-// 0.09016994375
+
+/// 0.09016994375
 const double kGoldenRatioDivide_5 = kGoldenRatioDivide_4 / _kGoldenRatio;
