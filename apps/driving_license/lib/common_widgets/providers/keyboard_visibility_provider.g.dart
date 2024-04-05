0f433b28168488939256d02ec897344693a3faf2
Victor Nguyen <victorpublic0000@gmail.com>
2024-04-05 09:56:22 +0700

    feat: add keyboardVisibility provider

---
+++ apps/driving_license/lib/common_widgets/providers/keyboard_visibility_provider.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'keyboard_visibility_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$keyboardVisibilityHash() =>
+    r'4e47898e372dc9b7946aa864b6e2d5f5c99c2d89';
+
+/// See also [keyboardVisibility].
+@ProviderFor(keyboardVisibility)
+final keyboardVisibilityProvider = AutoDisposeProvider<bool>.internal(
+  keyboardVisibility,
+  name: r'keyboardVisibilityProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$keyboardVisibilityHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef KeyboardVisibilityRef = AutoDisposeProviderRef<bool>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
