e3e360403dd8d70acf7001e9001beba46eff811a
Victor Nguyen <victorpublic0000@gmail.com>
2024-04-08 22:48:03 +0700

    feat: Add gradeExam() method to ExamsService

--- apps/driving_license/lib/features/exams/application/exams_service.dart
+++ apps/driving_license/lib/features/exams/application/exams_service.dart
@@ -4,6 +4,8 @@ import 'package:driving_license/features/exams/domain/exam.dart';
 import 'package:driving_license/features/licenses/data/providers/user_selected_license_provider.dart';
 import 'package:driving_license/features/licenses/domain/license.dart';
 import 'package:driving_license/features/questions/data/question/questions_repository.dart';
+import 'package:driving_license/features/questions/data/user_answer/user_answers_repository.dart';
+import 'package:driving_license/features/result/domain/test_result.dart';
 import 'package:driving_license/utils/datetime_formatter.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -43,6 +45,37 @@ class ExamsService {
 
   Stream<List<Exam>> watchExamsList() {
     return examsRepository.watchAllExamsByLicense(license);
+  }
+
+  FutureOr<void> gradeExam(Exam exam, UserAnswersMap userAnswers) {
+    final total = exam.questionDbIndexes.length;
+    int answered = 0;
+    int correct = 0;
+    int wrong = 0;
+
+    for (final questionId in exam.questionDbIndexes) {
+      final userAnswer = userAnswers[questionId];
+      if (userAnswer != null) {
+        answered++;
+
+        if (userAnswer.selectedAnswerIndex ==
+            userAnswer.questionMetadata.correctAnswerIndex) {
+          correct++;
+        } else {
+          wrong++;
+        }
+      }
+    }
+
+    return examsRepository.saveExamResult(
+      exam,
+      TestResult(
+        totalQuestions: total,
+        answeredQuestions: answered,
+        correctAnswers: correct,
+        wrongAnswers: wrong,
+      ),
+    );
   }
 }
 
