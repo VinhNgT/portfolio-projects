c353349378f52c156fa6de3c35807a594da084cc
Victor Nguyen <victorpublic0000@gmail.com>
2024-05-13 10:55:08 +0700

    feat(drv_lcn): add Logger

---
+++ apps/driving_license/lib/logging/logger_provider.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'logger_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$loggerHash() => r'e313b0897e07acdf3c2033b99dcdeb9a9ef8009b';
+
+/// See also [logger].
+@ProviderFor(logger)
+final loggerProvider = Provider<Logger>.internal(
+  logger,
+  name: r'loggerProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$loggerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef LoggerRef = ProviderRef<Logger>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
