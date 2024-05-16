615401044371f5609c48c4039769c60173fe4b4f
Victor Nguyen <victorpublic0000@gmail.com>
2024-05-16 21:08:42 +0700

    chore(drv_lcn): init firebase

--- apps/driving_license/lib/main.dart
+++ apps/driving_license/lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:driving_license/bootstrap.dart';
+import 'package:driving_license/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   // Firebase needs this to be called before runApp
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   final app = await Bootstrap.initApp();
   // ignore: missing_provider_scope
