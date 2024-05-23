3a112322982047795448dabb70ba8f52422f6fa9
Victor Nguyen <victorpublic0000@gmail.com>
2024-05-23 17:42:44 +0700

    feat(drv_lcn): add AdLayoutConstraints provider

---
+++ apps/driving_license/lib/backend/ads/ad_layout_constraints_provider.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'ad_layout_constraints_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$adLayoutConstraintsHash() =>
+    r'7a6eecc3e49ddb5f9eed4c2986b76f25f3651f52';
+
+/// See also [AdLayoutConstraints].
+@ProviderFor(AdLayoutConstraints)
+final adLayoutConstraintsProvider =
+    NotifierProvider<AdLayoutConstraints, Map<AdUnit, BoxConstraints>>.internal(
+  AdLayoutConstraints.new,
+  name: r'adLayoutConstraintsProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$adLayoutConstraintsHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$AdLayoutConstraints = Notifier<Map<AdUnit, BoxConstraints>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
