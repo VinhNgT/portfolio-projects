89ac0a4d8429ec29e5f391d035b0b6947d4dc8a7
Victor Nguyen <victorpublic0000@gmail.com>
2024-06-29 17:18:10 +0700

    feat(e_com): add support for dotenv

---
+++ apps/e_commerce/lib/backend/env/env.dart
@@ -0,0 +1,30 @@
+import 'package:e_commerce/backend/cache/domain/cache_config.dart';
+import 'package:logger/logger.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'env.g.dart';
+
+class Env {
+  final CacheConfig cacheConfig = () {
+    const cacheDuration = String.fromEnvironment('CACHE_DURATION_SECONDS');
+    const storageDuration =
+        String.fromEnvironment('CACHE_STORAGE_DURATION_SECONDS');
+    return CacheConfig(
+      cacheDurationSeconds: int.tryParse(cacheDuration),
+      storageDurationSeconds: int.tryParse(storageDuration),
+    );
+  }();
+
+  final Level loggerLevel = () {
+    const logLevel = String.fromEnvironment('LOG_LEVEL');
+    return Level.values.firstWhere(
+      (e) => e.name == logLevel,
+      orElse: () => Level.info,
+    );
+  }();
+}
+
+@Riverpod(keepAlive: true)
+Env env(EnvRef ref) {
+  return Env();
+}
---
+++ apps/e_commerce/lib/backend/env/env.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'env.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$envHash() => r'662aef8c10d812d5a97151cfc1141b088ef99eed';
+
+/// See also [env].
+@ProviderFor(env)
+final envProvider = Provider<Env>.internal(
+  env,
+  name: r'envProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$envHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef EnvRef = ProviderRef<Env>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
