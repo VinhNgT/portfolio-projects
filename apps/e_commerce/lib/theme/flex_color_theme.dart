643e7a75040096148a544c9afeb0dddbcb93a5eb
Victor Nguyen <victorpublic0000@gmail.com>
2024-07-10 23:40:23 +0700

    fix(e_com): disable _InputPadding

--- apps/e_commerce/lib/theme/flex_color_theme.dart
+++ apps/e_commerce/lib/theme/flex_color_theme.dart
@@ -83,6 +83,8 @@ class FlexColorTheme {
     useMaterial3: true,
     // To use the Playground font, add GoogleFonts package and uncomment
     // fontFamily: GoogleFonts.notoSans().fontFamily,
+  ).copyWith(
+    materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
   );
 
   /// NOT READY FOR USAGE, dark theme created by FlexColorScheme playground.
@@ -164,5 +166,7 @@ class FlexColorTheme {
     useMaterial3: true,
     // To use the Playground font, add GoogleFonts package and uncomment
     // fontFamily: GoogleFonts.notoSans().fontFamily,
+  ).copyWith(
+    materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
   );
 }
