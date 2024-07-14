64950be53e8851dbe192ff3bffde24fe306a722a
Victor Nguyen <victorpublic0000@gmail.com>
2024-07-14 22:45:51 +0700

    feat(e_com): add deadzone and simple bottom sheet

---
+++ apps/e_commerce/lib/common/ui/simple_bottom_sheet.dart
@@ -0,0 +1,85 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:e_commerce/common/widget_deadzone.dart';
+import 'package:e_commerce/constants/app_sizes.dart';
+import 'package:e_commerce/utils/context_extensions.dart';
+import 'package:flutter/material.dart';
+import 'package:material_symbols_icons/symbols.dart';
+
+class SimpleBottomSheet extends StatelessWidget {
+  const SimpleBottomSheet({
+    super.key,
+    required this.child,
+    this.mainAxisSize = MainAxisSize.min,
+    this.addSafeArea = true,
+  });
+
+  final Widget child;
+  final MainAxisSize mainAxisSize;
+  final bool addSafeArea;
+
+  @override
+  Widget build(BuildContext context) {
+    return WidgetDeadzone(
+      deadzone: EdgeInsets.only(
+        bottom: context.systemGestureInsets.bottom,
+      ),
+      child: Stack(
+        children: [
+          _addSafeArea(
+            isEnable: addSafeArea,
+            child: Column(
+              mainAxisSize: mainAxisSize,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(top: kSize_8),
+                  child: _Handle(),
+                ),
+                Flexible(child: child),
+              ],
+            ),
+          ),
+          const Positioned(
+            top: kSize_12,
+            right: kSize_4,
+            child: _CloseButton(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _addSafeArea({required Widget child, required bool isEnable}) {
+    return isEnable ? SafeArea(child: child) : child;
+  }
+}
+
+class _Handle extends StatelessWidget {
+  const _Handle();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: kSize_32,
+      height: kSize_4,
+      decoration: BoxDecoration(
+        color: context.colorScheme.outline,
+        borderRadius: const BorderRadius.all(Radius.circular(100)),
+      ),
+    );
+  }
+}
+
+class _CloseButton extends StatelessWidget {
+  const _CloseButton();
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(
+        Symbols.close,
+        color: context.colorScheme.onSurfaceVariant,
+      ),
+      onPressed: () async => context.maybePop(),
+    );
+  }
+}
