3ad5ed4195148298042b027fb6e3febdb53ee80e
Victor Nguyen <victorpublic0000@gmail.com>
2024-07-19 21:40:59 +0700

    feat(e_com): add kSize_36

--- apps/e_commerce/lib/constants/app_sizes.dart
+++ apps/e_commerce/lib/constants/app_sizes.dart
@@ -12,6 +12,7 @@ const kSize_16 = 16.0;
 const kSize_20 = 20.0;
 const kSize_24 = 24.0;
 const kSize_32 = 32.0;
+const kSize_36 = 36.0;
 const kSize_48 = 48.0;
 const kSize_56 = 56.0;
 const kSize_60 = 60.0;
