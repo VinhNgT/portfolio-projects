22f5433908c160ccbdfdd42290b43157d6914c5a
VinhNgT <victorpublic0000@gmail.com>
2024-10-13 10:52:20 +0700

    chore: temporary change e_commerce folder name

--- melos_workspace/apps/e_commerce/lib/utils/date_formatter.dart
+++
@@ -1,8 +0,0 @@
-import 'package:intl/intl.dart';
-
-abstract class DateFormatter {
-  static String format(DateTime date) {
-    final DateFormat formatter = DateFormat('dd/MM/yyyy');
-    return formatter.format(date);
-  }
-}
