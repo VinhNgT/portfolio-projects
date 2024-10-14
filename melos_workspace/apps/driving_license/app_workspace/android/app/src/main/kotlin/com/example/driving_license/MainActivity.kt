f6ac515a43d393f7c006475fb08e39881c0003fa
VinhNgT <victorpublic0000@gmail.com>
2024-10-14 22:04:14 +0700

    refactor(drv_lcn): recreate android folder

--- melos_workspace/apps/driving_license/app_workspace/android/app/src/main/kotlin/com/example/driving_license/MainActivity.kt
+++ melos_workspace/apps/driving_license/app_workspace/android/app/src/main/kotlin/com/example/driving_license/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.driving_license
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
--- melos_workspace/apps/driving_license/app_workspace/android/app/src/main/kotlin/com/vikapps/driving_license/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.vikapps.driving_license
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
